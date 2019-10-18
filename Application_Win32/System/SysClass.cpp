c655cc8fe793ab665c97acff654b83396d8e6a3f
Ruben Young <rubeny484@gmail.com>
2019-10-18 12:24:00 -0400

    Framework for System-class

---
+++ Application_Win32/System/SysClass.cpp
@@ -0,0 +1,7 @@
+/*----------------------------------------------
+Ruben Young (rubenaryo@gmail.com)
+Date : 2019/10
+Description : Implementation for System class methods
+----------------------------------------------*/
+
+#include "SysClass.h"
---
+++ Application_Win32/System/SysClass.h
@@ -0,0 +1,61 @@
+/*----------------------------------------------
+Ruben Young (rubenaryo@gmail.com)
+Date : 2019/10
+Description : Interface (header) for System class
+Thanks to http://www.rastertek.com/gl40tut02.html
+----------------------------------------------*/
+#ifndef SYSCLASS_H
+#define SYSCLASS_H
+
+// Speed up compilation by disabling certain includes
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+// Include standard windows headers
+#include <Windows.h>
+#include <tchar.h>
+
+// Use Unicode character representations
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+// WNDPROC function prototype
+static LRESULT CALLBACK WndProc(_In_ HWND, _In_ UINT, _In_ WPARAM, _In_ LPARAM);
+
+// Class definition for System class
+class SysClass
+{
+public:
+    SysClass();
+    SysClass(const SysClass&);
+    ~SysClass();
+
+    bool Init();
+    void Shutdown();
+    void Run();
+
+    // Message Handling Callback for Windows
+    LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+private:
+    bool Frame();
+    bool InitWindows();
+    void ShutdownWindows();
+
+private:
+    // Windows Fields
+    LPCWSTR m_appName;
+    HINSTANCE m_hInstance;
+    HWND m_hwnd;
+
+    ///TODO: Pointers to other app systems  
+};
+
+// Pointer-type definition
+typedef SysClass* pSysClass;
+
+// Global Application Handle
+static pSysClass AppHandle = nullptr;
+
+#endif
