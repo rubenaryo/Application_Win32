5a9d60409e7ed086d8dfdf83334e25df6a7daa3c
Ruben Young <rubeny484@gmail.com>
2019-11-10 19:42:56 -0500

    Skeleton code for D3D11 Class Framework
    Missing Implementation

---
+++ Application_Win32/D3DClass.cpp
@@ -0,0 +1,9 @@
+/*----------------------------------------------
+Ruben Young (rubenaryo@gmail.com)
+Date : 2019/11
+Description : Implementation of base Direct3D functionality
+----------------------------------------------*/
+#include "D3DClass.h"
+
+
+using namespace System::Graphics;
---
+++ Application_Win32/D3DClass.h
@@ -0,0 +1,68 @@
+/*----------------------------------------------
+Ruben Young (rubenaryo@gmail.com)
+Date : 2019/11
+Description : Interface for using Direct3D 11
+----------------------------------------------*/
+#ifndef D3DCLASS_H
+#define D3DCLASS_H
+
+// Link Direct3D Libraries
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+// DirectX necessary includes
+#include <d3d11.h>
+#include <directxmath.h>
+
+namespace System {
+namespace Graphics {
+
+class Direct3DClass
+{
+public:
+    Direct3DClass();
+    ~Direct3DClass();
+
+    bool Init();
+    void OnResize();
+    void UpdateWindowState();
+    void Update(float dt);
+    void Draw();
+
+protected:
+    void CalculateFrameStats();
+
+protected:
+    // Client Width, Height, and HWND (Window Handle)
+    int m_ClientWidth;
+    int m_ClientHeight;
+    HWND m_hMainWindow;
+
+    // States of the window
+    bool m_Paused;
+    bool m_Minimized;
+    bool m_Maximized;
+    bool m_ResizeDragging;
+
+    // Direct3D-Specific fields
+    bool m_Enable4xMSAA;
+    unsigned int m_4xMSAAQuality;
+
+    ID3D11Device* m_pD3DDevice;
+    ID3D11DeviceContext* m_pD3DImmediateContext;
+    IDXGISwapChain* m_pSwapChain;
+    ID3D11Texture2D* m_pDepthStencilBuffer;
+    ID3D11RenderTargetView* m_pRenderTargetView;
+    ID3D11DepthStencilView* m_pDepthStencilView;
+    D3D11_VIEWPORT m_ScreenViewport;
+    D3D_DRIVER_TYPE m_D3DDriverType;
+
+};
+
+}}
+
+
+
+
+#endif
